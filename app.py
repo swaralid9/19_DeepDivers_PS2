aa0bf170acfe1e50cf10679d6e85408c1f1a8b0b
SwaraliDhobale <swaralidhobale@gmail.com>
2024-03-24 18:03:34 +0530

    update-successful

--- app.py
+++ app.py
@@ -4,13 +4,6 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
-import os
-import random
-from flask import Flask, render_template, request
-from tensorflow.keras.preprocessing.image import img_to_array, ImageDataGenerator, load_img
-import numpy as np
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 
 app = Flask(__name__)
 thresholds = {
@@ -46,69 +39,9 @@ def predict():
 
         return render_template('predict.html', prediction=prediction[0])
 
-dataset_path = 'PlantDisease'  
-
-
-def get_train_generator():
-    train_path = os.path.join(dataset_path, 'train')
-    train_datagen = ImageDataGenerator(rescale=1./255)
-    train_generator = train_datagen.flow_from_directory(
-        train_path,
-        target_size=(100, 100),
-        batch_size=32,
-        class_mode='binary'  # Assuming two classes: healthy and unhealthy
-    )
-    return train_generator
-
-def classify_random_image():
-    train_generator = get_train_generator()
-
-    model = Sequential([
-        Conv2D(32, (3, 3), activation='relu', input_shape=(100, 100, 3)),
-        MaxPooling2D((2, 2)),
-        Conv2D(32, (3, 3), activation='relu'),
-        MaxPooling2D((2, 2)),
-        Flatten(),
-        Dense(64, activation='relu'),
-        Dense(1, activation='sigmoid')  # Binary classification: healthy or unhealthy
-    ])
-
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-    model.fit(train_generator, epochs=15)
-
-    random_class = random.choice(['healthy', 'unhealthy'])
-    class_path = os.path.join(dataset_path, 'train', random_class)
-    if os.listdir(class_path):
-        random_image_filename = random.choice(os.listdir(class_path))
-        random_image_path = os.path.join(class_path, random_image_filename)
-        random_image = load_img(random_image_path, target_size=(100, 100))
-
-        random_image_array = img_to_array(random_image)
-        random_image_array = random_image_array.reshape(1, 100, 100, 3) / 255.0
-
-        prediction = model.predict(random_image_array)
-        predicted_class = 'healthy' if prediction < 0.5 else 'unhealthy'
-
-        predictions_data = {
-            'image_path': random_image_path,
-            'predicted_class': predicted_class,
-        }
-    else:
-        predictions_data = {'image_path': '', 'predicted_class': 'Unknown'}
-
-    return predictions_data
-
 @app.route('/')
-def index():
-    predictions_data = {'image_path': '', 'predicted_class': 'Unknown'}
-    return render_template('index.html', predictions_data=predictions_data)
-
-@app.route('/results')
-def results():
-    predictions_data = classify_random_image()
-    return render_template('results.html', predictions_data=predictions_data)
-
+def home():
+    return render_template('index.html')
 
 @app.route('/input')
 def input():
@@ -122,9 +55,17 @@ def health():
 def metrics():
     return render_template('metrics.html')
 
+@app.route('/image')
+def image():
+    return render_template('image.html')
+
 @app.route('/blogs')
 def blogs():
     return render_template('blogs.html')
+
+@app.route('/air')
+def air():
+    return render_template('air.html')
 
 def generate_recommendations(data):
     recommendations = []
@@ -148,20 +89,5 @@ def recommend():
     
     return render_template('recommendations.html', recommendations=recommendations)
 
-
 if __name__ == '__main__':
-       # model and train_generator
-    model = Sequential([
-        Conv2D(32, (3, 3), activation='relu', input_shape=(100, 100, 3)),
-        MaxPooling2D((2, 2)),
-        Conv2D(32, (3, 3), activation='relu'),
-        MaxPooling2D((2, 2)),
-        Flatten(),
-        Dense(64, activation='relu'),
-        Dense(7, activation='softmax')  # 7 classes including 'Endangered_Animals'
-    ])
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-    train_generator = get_train_generator()
-
     app.run(debug=True)
